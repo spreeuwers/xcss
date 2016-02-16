5794272ccc15c6fed93ff39871ce1f3e700e7509
spreeuwers <e.spreeuwers@gmail.com>
2016-02-16 14:54:42 +0100

    Update xcss.js

--- xcss.js
+++ xcss.js
@@ -1,38 +1,55 @@
-/**
- * Created by eddy spreeuwers on 15-02-16.
- */
-
-window.setTimeout(inheritCSSRules, 100);
-
-function inheritCSSRules() {
-
+(function inheritCSSRules() {
+    "use strict";
     var allCSSRules = {};
+    var visitedRules = {};
+    var EXTENDS_EXPR = /\s+extends\s+/i;
 
     //collect all style rules
-
-
     collectRules(document);
-    console.log(allCSSRules);
+    //console.log(allCSSRules);
+
     var styleSheet = addNewStylesheet();
 
-    Object.keys(allCSSRules).forEach(
-        function(selector){
-            var inherit = selector.trim().split(/\s+extends\s+/i);
-            if (inherit.length === 2){
-                var dest = inherit[0].trim();
+    extendRules(allCSSRules);
 
-                var from = inherit[1].trim().split(/\s*,\s*/).map(
-                    function(sel){
-                        return (allCSSRules[sel]) ? allCSSRules[sel].style.cssText : '';
-                    }).join(' ');
-                var self = allCSSRules[selector];
-                styleSheet.insertRule(dest + '{' + self.style.cssText + from + '}', styleSheet.cssRules.length);
+    console.log(styleSheet);
+
+
+    function extendRules(cssRules) {
+        Object.keys(cssRules).forEach(
+            function (selector) {
+
+                if (visitedRules[selector]) {
+                    //console.log('skipping visited selector: ' + selector);
+                    return;
+                }
+                visitedRules[selector] = cssRules[selector];
+
+                var inherit = selector.trim().split(EXTENDS_EXPR);
+                if (inherit.length === 2) {
+                    console.log('processing selector: ' + selector);
+                    var dest = inherit[0].trim();
+                    var from = inherit[1].trim().split(/\s*,\s*/).map(
+                        function (fromSelector) {
+                            //resolve fromSelector when not found in currenttly resolved rules
+                            if (!cssRules[fromSelector]) {
+                                console.log('resolving rule fromSelector: ' + fromSelector);
+                                extendRules(cssRules);
+                            }
+                            return (cssRules[fromSelector]) ? cssRules[fromSelector].style.cssText : '';
+                        }
+                    ).join(' ');
+                    var self = cssRules[selector];
+                    console.log('adding rule: ' + dest + '{' + self.style.cssText + from + '}');
+                    var idx = styleSheet.insertRule(dest + '{' + self.style.cssText + from + '}', styleSheet.cssRules.length);
+                    cssRules[dest] = styleSheet.cssRules[idx];
+                }
             }
-        }
-    );
+        );
+    }
 
     function collectRules(container) {
-        [].slice.call(container.styleSheets||[]).forEach(
+        [].slice.call(container.styleSheets || []).forEach(
             function (styleSheet) {
                 [].slice.call(styleSheet.cssRules || []).forEach(
                     function (cssRule) {
@@ -44,7 +61,7 @@ function inheritCSSRules() {
         );
     }
 
-    function addNewStylesheet () {
+    function addNewStylesheet() {
         var styleEl = document.createElement('style'),
             styleSheet;
 
@@ -54,4 +71,6 @@ function inheritCSSRules() {
         // Grab style sheet
         return styleEl.sheet;
     }
-}
+
+    document.addEventListener('DOMContentLoaded', inheritCSSRules);
+})();
